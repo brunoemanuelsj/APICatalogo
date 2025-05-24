78ca2d0274829d269b0ea6c9c3f65b5577702f3e
Bruno Jaime <emanuelbrunosj@gmail.com>
2025-05-24 18:40:19 -0300

    controllers

--- Migrations/20250522034856_PopulaProdutos.cs
+++ Migrations/20250522034856_PopulaProdutos.cs
@@ -10,13 +10,26 @@ namespace APICatalogo.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder mb)
         {
-            mb.Sql(@"Insert into Produtos(""Nome"", ""Descricao"", )");
+            mb.Sql(@"
+                INSERT INTO produtos (""Nome"", ""Descricao"", ""Preco"", ""ImagemUrl"", ""Estoque"", ""DataCadastro"", ""CategoriaId"")
+                VALUES ('Coca-Cola Diet', 'Refrigerante de Cola 350 ml', 5.45, 'cocacola.jpg', 50, NOW(), 1);
+            ");
+
+            mb.Sql(@"
+                INSERT INTO produtos (""Nome"", ""Descricao"", ""Preco"", ""ImagemUrl"", ""Estoque"", ""DataCadastro"", ""CategoriaId"")
+                VALUES ('Lanche de Atum', 'Lanche de Atum com maionese', 8.50, 'atum.jpg', 10, NOW(), 2);
+            ");
+
+            mb.Sql(@"
+                INSERT INTO produtos (""Nome"", ""Descricao"", ""Preco"", ""ImagemUrl"", ""Estoque"", ""DataCadastro"", ""CategoriaId"")
+                VALUES ('Pudim 100 g', 'Pudim de leite condensado 100g', 6.75, 'pudim.jpg', 20, NOW(), 3);
+            ");
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder mb)
         {
-
+            mb.Sql(@"DELETE FROM ""Produtos"";");
         }
     }
 }
